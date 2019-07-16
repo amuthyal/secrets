321f69574abc8c71a213bdbefcca0380e6cf2b4e
akhila <amuthyal@asu.edu>
2019-07-16 01:32:14 -0700

    bcrypt hashing

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ require('dotenv').config();
 
 const bodyParser = require("body-parser");
 const express = require("express");
-const ejs= require('ejs');
+const ejs = require('ejs');
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //to connect to mongodb
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 // New user database, so we are creating schema
 
@@ -29,57 +35,65 @@ const userSchema = new mongoose.Schema({
 // use the plugin before u create a new mongoose model cuz, we are passing a userSchema in model
 
 
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+app.post("/register", function(req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else {
-      res.render("secrets");
-    }
-  });
+
+
 });
 
-app.post("/login", function(req,res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
       console.log(err);
-    } else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }else{
-          res.send("wrong password");
-        }
-      }else{
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else{
+            res.send("wrong password");
+          }
+        });
+      } else {
         res.send("cant find the user");
       }
     }
   });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });

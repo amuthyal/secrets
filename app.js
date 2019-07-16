48d32b33663352a8684a7d01c8b33de4ec8ea9b3
akhila <amuthyal@asu.edu>
2019-07-16 00:44:20 -0700

    hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const ejs= require('ejs');
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,7 +24,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 // use the plugin before u create a new mongoose model cuz, we are passing a userSchema in model
 
@@ -47,7 +48,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -60,7 +61,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
@@ -69,7 +70,11 @@ app.post("/login", function(req,res){
       if(foundUser){
         if(foundUser.password === password){
           res.render("secrets");
+        }else{
+          res.send("wrong password");
         }
+      }else{
+        res.send("cant find the user");
       }
     }
   });
